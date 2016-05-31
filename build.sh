3b07f50271865f046fc51750fae284aa089d442f
Jonathan Lurie <lurie.jo@gmail.com>
2016-05-30 20:40:16 -0400

    ready to be built

---
+++ build.sh
@@ -0,0 +1,15 @@
+electron-packager . "Duskr-Prelude" \
+    –version-string.CompanyName="Duskr" \
+    –version-string.ProductName="Duskr-Prelude" \
+    –version-string.ProductVersion="0.1.0" \
+    --icon="icon.icns" \
+    --platform=darwin \
+    --asar=true \
+    --arch=x64 \
+    --out="build/" \
+    --overwrite \
+    --ignore=".DS_Store" \
+    --ignore=".gitignore" \
+    --ignore=".git" \
+    --ignore="build.sh" \
+    --ignore="build" \
